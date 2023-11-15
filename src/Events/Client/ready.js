10429eddfd7f5138696dd5d258d44ca0ae5d91d2
Nicolas <yncampos@ucundinamarca.edu.co>
2023-11-14 21:06:29 -0500

    Conexión a la base de datos MongoDB

--- src/Events/Client/ready.js
+++ src/Events/Client/ready.js
@@ -1,7 +1,35 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const config = require('../../../config.json');
+require('colors');
+
+const clientDB = new MongoClient(config.MONGODB_URL_ENCODED, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client){
+    async execute(client) {
+
+        try {
+            await clientDB.connect();
+
+            await clientDB.db("admin").command({ ping: 1 });
+
+            console.log("[MONGODB] is connected sucessfully :D".green);
+
+        } catch (error) {
+            console.log(error.red)
+
+        } finally {
+            // Ensures that the client will close when you finish/error
+            await clientDB.close();
+
+        }
         console.log(`The ${client.user.username} is online`);
     }
 }
